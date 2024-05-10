2a4c5bfbd5e6836dcb20d2d924ca4d5ce926266f
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-05-09 22:13:01 -0300

    creating EnvModule

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ConfigService } from '@nestjs/config'
-import { Env } from './env'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
 
-  // By default on Nest all .env variables can be undefined.
-  // But ConfigService<Env, true> means the type of ConfigService is Env and
-  // WasValidated=true so none of the variables inside will be undefined
-  const configService = app.get<ConfigService<Env, true>>(ConfigService)
-  const port = configService.get('PORT', { infer: true })
+  const env = app.get(EnvService)
+  const port = env.get('PORT')
 
   await app.listen(port)
 }
